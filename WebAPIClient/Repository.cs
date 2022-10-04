46a4dddb6cb783bf5865acc5bde9de9d2a68eafb
Parita Shah <PARITASHAH@COLLEGE.HARVARD.EDU>
2022-10-03 21:51:29 -0400

    added filter, delete, update functionality

--- WebAPIClient/Repository.cs
+++ WebAPIClient/Repository.cs
@@ -4,27 +4,18 @@ using System.Text.Json.Serialization;
 
 namespace WebAPIClient
 {
-    public class Repository
+    public record Article 
     {
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
+        [JsonPropertyName("id")]
+        public int Id {get; set;} 
 
-        [JsonPropertyName("description")]
-        public string Description { get; set; }
-
-        [JsonPropertyName("html_url")]
-        public Uri GitHubHomeUrl { get; set; }
-
-        [JsonPropertyName("homepage")]
-        public Uri Homepage { get; set; }
-
-        [JsonPropertyName("watchers")]
-        public int Watchers { get; set; }
-
-        [JsonPropertyName("pushed_at")]
-        public string JsonDate { get; set; }
-
-        public DateTime LastPush =>
-            DateTime.ParseExact(JsonDate, "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+        [JsonPropertyName("title")]
+        public string ? Title { get; set; }
+        
+        [JsonPropertyName("author")]
+        public string ? Author {get; set; }
+        
+        [JsonPropertyName("date")]
+        public string ? Date {get; set;}
     }
 }
--- WebApi/Db.cs
+++ WebApi/Db.cs
@@ -5,8 +5,7 @@ namespace Database.DB;
    public int Id {get; set;} 
    public string ? Title { get; set; }
    public string ? Author {get; set; }
-
-    public string ? Date {get; set;}
+   public string ? Date {get; set;}
  }
 
   public class ArticleDB
@@ -23,6 +22,31 @@ namespace Database.DB;
      return _Articles;
    } 
 
+    // get filtered articles
+   public static List<Article> ? GetFilteredArticles(string field, string keyword) 
+   {
+      List<Article> filteredArticles = new List<Article>();
+
+      if (_Articles.Count != 0) {
+        foreach (Article article in _Articles)
+        {
+          if (field == "title" && article.Title != null && article.Title.Contains(keyword))
+          {
+            filteredArticles.Add(article);
+          }
+          else if (field == "author"  && article.Author != null && article.Author.Contains(keyword))
+          {
+            filteredArticles.Add(article);
+          }
+          else if (field == "date" && article.Date != null && article.Date.Contains(keyword))
+          {
+            filteredArticles.Add(article);
+          }
+        }
+      }
+      return filteredArticles;
+   } 
+
    public static Article ? GetArticle(int id) 
    {
     return _Articles.SingleOrDefault(a => a.Id == id);
@@ -37,9 +61,58 @@ namespace Database.DB;
      return newArticle;
    }
 
+    public static Article ? UpdateArticle(int id, string field, string change) 
+    {
+      Article ? articleupdate = _Articles.SingleOrDefault(a => a.Id == id);
+      if (articleupdate != null) 
+      {
+        if (field.ToLower() == "title") 
+        {
+          articleupdate.Title = change;
+        }
+        else if (field.ToLower() == "author") 
+        {
+          articleupdate.Author = change;
+        }
+        else if (field.ToLower() == "date") 
+        {
+          articleupdate.Date = change;
+        }
+      }
+      return articleupdate;
+    }
+
+    public static Article ? UpdateEntireArticle(int id, string title, string date, string author) 
+    {
+      Article ? articleupdate = _Articles.SingleOrDefault(a => a.Id == id);
+      if (articleupdate != null) 
+      {
+        articleupdate.Title = title;
+        articleupdate.Date= date;
+        articleupdate.Author = author;
+      }
+      return articleupdate;
+    }
    public static void RemoveArticle(int id)
    {
      _Articles = _Articles.FindAll(a => a.Id != id).ToList();
      _numArticles -= 1;
+   }
+
+   public static void RemoveArticles(string field, string keyword)
+   {
+    if (field.ToLower() == "title")
+    {
+      _Articles = _Articles.FindAll(a => a.Title != keyword).ToList();
+    }
+    else if (field.ToLower() == "author")
+    {
+       _Articles = _Articles.FindAll(a => a.Author != keyword).ToList();
+    }
+    else if (field.ToLower() == "date")
+    {
+       _Articles = _Articles.FindAll(a => a.Date != keyword).ToList();
+    }
+    _numArticles = _Articles.Count;
    }
  }
